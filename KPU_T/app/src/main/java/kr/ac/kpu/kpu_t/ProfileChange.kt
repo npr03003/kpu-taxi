9e24bed5ce2b9abc41d3924b799a83edd4ca73be
npr03003 <npr03003@naver.com>
2020-07-30 11:44:47 +0900

    정우

---
+++ KPU_T/app/src/main/java/kr/ac/kpu/kpu_t/ProfileChange.kt
@@ -0,0 +1,12 @@
+package kr.ac.kpu.kpu_t
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileChange : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_change)
+    }
+}
